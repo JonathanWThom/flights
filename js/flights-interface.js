3cadcec7291685fbb6b850e70e11927e309f9a9a
Jonathan Thom and Tracie Weitzman <tracie@>
2017-01-05 11:55:34 -0800

    hide flight list

--- js/flights-interface.js
+++ js/flights-interface.js
@@ -2,6 +2,7 @@ var Flights = require('./../js/flights.js').flightsModule;
 
 function displayPrice(price, flightNumber, link, departure, duration, cabin) {
   $('#showFlights').show();
+  $('.flight-list').show();
   $('#showFlights tbody').append('<tr><td>' + price + '</td><td>' + flightNumber + '</td><td>' + link + '</td><td>' + departure + '</td><td>' + duration + '</td><td>' + cabin + '</td></tr>');
 
 }
