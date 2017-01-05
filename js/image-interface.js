a5ef1622b05728877aa7afbd7eb2495b6ed1bf3c
Jonathan Thom and Tracie Weitzman <jonathan@>
2017-01-05 14:41:46 -0800

    add carousel and go round and round

--- js/image-interface.js
+++ js/image-interface.js
@@ -1,13 +1,21 @@
 var Image = require('./../js/image.js').imageModule;
 
-function displayImage(farmId, serverId, photoId, secret) {
-  var link = "https://farm" + farmId + ".staticflickr.com/" + serverId + "/" + photoId + "_" + secret + "_n.jpg";
-  $('#showImage').html("<img id='photo-size' src='" + link + "'>");
+function displayImage(farmId, serverId, photoId, secret, number) {
+  var link = "https://farm" + farmId + ".staticflickr.com/" + serverId + "/" + photoId + "_" + secret + "_q.jpg";
+  // $('#showImage').append("<img id='photo-size' src='" + link + "'>");
+
+  $('.carousel.carousel-slider').append("<a class='carousel-item' href='#" + number + "!'><img src='" + link + "'></a>");
+
+  var imageTest = "<a class='carousel-item' href='#" + number + "!'><img id='photo-size' src='" + link + "'></a>";
+  console.log(imageTest);
+
 }
 
 $(document).ready(function(){
+
   $('#getFlights').click(function() {
     var destination = $('#destination').val();
     Image.findImage(destination, displayImage);
+
   });
 });
--- js/image.js
+++ js/image.js
@@ -16,14 +16,26 @@ Image.findImage = function(destination, displayImage) {
     location = "El Potrero Chico";
   }
 
-  $.get('https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=' + flickrKey + '&tags=' + location + '&sort=interestingness-desc&per_page=2&page=1&format=json&nojsoncallback=1')
+  $.get('https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=' + flickrKey + '&tags=' + location + '&sort=interestingness-desc&per_page=3&page=1&format=json&nojsoncallback=1')
     .then(function(response) {
-      console.log(response);
-      farmId = response.photos.photo[1].farm;
-      serverId = response.photos.photo[1].server;
-      photoId = response.photos.photo[1].id;
-      secret = response.photos.photo[1].secret;
-      displayImage(farmId, serverId, photoId, secret);
+      for (i = 0; i < 3; i++) {
+        var farmId = response.photos.photo[i].farm;
+        var serverId = response.photos.photo[i].server;
+        var photoId = response.photos.photo[i].id;
+        var secret = response.photos.photo[i].secret;
+        var number;
+        if (i === 0 ) {
+          number = "one";
+        } else if (i === 1) {
+          number = "two";
+        } else if (i === 2) {
+          number = "three";
+        }
+        console.log(number);
+        displayImage(farmId, serverId, photoId, secret, number);
+
+      }
+      $('.carousel.carousel-slider').carousel({full_width: true});
     }).fail(function(error) {
       //do nothing
     });
